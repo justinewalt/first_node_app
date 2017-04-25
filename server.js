67237c93f2691ac6e5527de6da168a0a87393b61
Justin Ewalt <justinewalt@gmail.com>
2017-04-24 18:53:11 -0600

    second commit

--- server.js
+++ server.js
@@ -1,7 +1,27 @@
 var http = require('http');
+var fs = require('fs');
+var Emitter = require('events').EventEmitter;
+var logger = new Emitter();
+var server = http.createServer();
+
+logger.on('error', function(message) {
+    console.log('ERROR: ' + message);
+});
+
 http.createServer( function(request, response) {
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end('My first node app');
+    fs.readFile('index.html', function(err, content) {
+        if (err) {
+            response.writeHead(500, 'text/plain');
+            logger.emit('error', err);
+            response.end(err);
+        } else {
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.end(content)};
+    });
 }).listen(8080);
+
+server.on('close', function() {
+    console.log("Server is down, please try again later");
+});
 
 console.log("Server running at http://localhost:8080/");
